9f2b2532d8daf5da5087640278ca9fc89cccbef6
mariath <maria.theodo@gmail.com>
2017-01-23 12:07:08 +0200

    finish project

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,20 @@
 function multiplesOfThreeAndFive() {
 	var sum = 0;
+
 	for (var i = 1; i < 1000; i++) {
 		if (i % 3 == 0 || i % 5 == 0) {
 			sum += i;
 		}
 	}
 	document.getElementById("answer1").innerHTML = "The answer is " + sum;
+	document.getElementById("answer1").style.backgroundColor = "#adc4cc";
 }
 
 function evenFibonacciNumbers() {
 	var sum = 0;
 	var i = 1;
 	var f = fibonacci(1);
+
 	while (f <= 4000000) {
 		if (f % 2 == 0) {
 			sum += f;
@@ -19,6 +22,7 @@ function evenFibonacciNumbers() {
 	f = fibonacci(++i);
 	}
 	document.getElementById("answer2").innerHTML = "The answer is " + sum;
+	document.getElementById("answer2").style.backgroundColor = "#adc4cc";
 }
 
 function fibonacci(x) {
@@ -30,6 +34,3 @@ function fibonacci(x) {
 	}
 }
 
-function largestPrimeFactor() {
-	
-}
